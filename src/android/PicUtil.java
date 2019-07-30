79a7660189b2d36435de85236de87cb5a8643cfa
xxy <xxyangyy@163.com>
2019-07-30 14:54:36 +0800

    修改android工具类

--- src/android/PicUtil.java
+++ src/android/PicUtil.java
@@ -9,12 +9,16 @@ import android.media.ExifInterface;
 import android.util.Base64;
 import android.util.Log;
 
+import org.apache.cordova.LOG;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class PicUtil {
 
@@ -24,126 +28,28 @@ public class PicUtil {
 
     public static String compressImage(String filePath, String type) {
 
-        Bitmap scaledBitmap = null;
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-
-//      by setting this field as true, the actual bitmap pixels are not loaded in the memory. Just the bounds are loaded. If
-//      you try the use the bitmap here, you will get null.
-        options.inJustDecodeBounds = true;
-        Bitmap bmp = BitmapFactory.decodeFile(filePath, options);
-
-        int actualHeight = options.outHeight;
-        int actualWidth = options.outWidth;
-
-//      max Height and width values of the compressed image is taken as 816x612
-
-        float maxHeight = 816.0f;
-        float maxWidth = 612.0f;
-        float imgRatio = actualWidth / actualHeight;
-        float maxRatio = maxWidth / maxHeight;
-
-//      width and height values are set maintaining the aspect ratio of the image
-
-        if (actualHeight > maxHeight || actualWidth > maxWidth) {
-            if (imgRatio < maxRatio) {
-                imgRatio = maxHeight / actualHeight;
-                actualWidth = (int) (imgRatio * actualWidth);
-                actualHeight = (int) maxHeight;
-            } else if (imgRatio > maxRatio) {
-                imgRatio = maxWidth / actualWidth;
-                actualHeight = (int) (imgRatio * actualHeight);
-                actualWidth = (int) maxWidth;
-            } else {
-                actualHeight = (int) maxHeight;
-                actualWidth = (int) maxWidth;
-
+        FileInputStream objFileIS = null;
+        try {
+            objFileIS = new FileInputStream(filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ByteArrayOutputStream objByteArrayOS = new ByteArrayOutputStream();
+        byte[] byteBufferString = new byte[1024];
+        try {
+            for (int readNum; (readNum = objFileIS.read(byteBufferString)) != -1; ) {
+                objByteArrayOS.write(byteBufferString, 0, readNum);
             }
-        }
-
-//      setting inSampleSize value allows to load a scaled down version of the original image
-
-        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
-
-//      inJustDecodeBounds set to false to load the actual bitmap
-        options.inJustDecodeBounds = false;
-
-//      this options allow android to claim the bitmap memory if it runs low on memory
-        options.inPurgeable = true;
-        options.inInputShareable = true;
-        options.inTempStorage = new byte[16 * 1024];
-
-        try {
-//          load the bitmap from its path
-            bmp = BitmapFactory.decodeFile(filePath, options);
-        } catch (OutOfMemoryError exception) {
-            exception.printStackTrace();
-
-        }
-        try {
-            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888);
-        } catch (OutOfMemoryError exception) {
-            exception.printStackTrace();
-        }
-
-        float ratioX = actualWidth / (float) options.outWidth;
-        float ratioY = actualHeight / (float) options.outHeight;
-        float middleX = actualWidth / 2.0f;
-        float middleY = actualHeight / 2.0f;
-
-        Matrix scaleMatrix = new Matrix();
-        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
-
-        Canvas canvas = new Canvas(scaledBitmap);
-        canvas.setMatrix(scaleMatrix);
-        canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
-
-//      check the rotation of the image and display it properly
-        ExifInterface exif;
-        try {
-            exif = new ExifInterface(filePath);
-
-            int orientation = exif.getAttributeInt(
-                ExifInterface.TAG_ORIENTATION, 0);
-            Log.d("EXIF", "Exif: " + orientation);
-            Matrix matrix = new Matrix();
-            if (orientation == 6) {
-                matrix.postRotate(90);
-                Log.d("EXIF", "Exif: " + orientation);
-            } else if (orientation == 3) {
-                matrix.postRotate(180);
-                Log.d("EXIF", "Exif: " + orientation);
-            } else if (orientation == 8) {
-                matrix.postRotate(270);
-                Log.d("EXIF", "Exif: " + orientation);
-            }
-            scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0,
-                scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix,
-                true);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        if (type.toLowerCase().contains("png")) {
-            scaledBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        } else {
-            scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-        }
 
-        byte[] datas = baos.toByteArray();
-        Log.e("size", (datas.length / 1024) + "");
-        return Base64.encodeToString(datas,Base64.DEFAULT);
 
+        String fileData =  Base64.encodeToString(objByteArrayOS.toByteArray(), Base64.DEFAULT).replaceAll("\r|\n","");
+        return fileData;
     }
 
     public static String encodeBase64File(String path) throws Exception {
-//        File file = new File(path);
-//        FileInputStream inputFile = new FileInputStream(file);
-//        byte[] buffer = new byte[(int)file.length()];
-//        inputFile.read(buffer);
-//        inputFile.close();
-//        return new BASE64Encoder().encode(buffer);
-
         String base64 = null;
         InputStream in = null;
         try {
